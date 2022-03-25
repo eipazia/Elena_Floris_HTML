76667f1de8d7d31c3f48f75abec7af0841f74744
eipazia <mandelbrot2014.ef@gmail.com>
2022-03-25 15:24:12 +0100

    Commit Finale

---
+++ Storage.js
@@ -0,0 +1,18 @@
+﻿////function getStorage() {
+
+////    let storageName = (document.getElementById("local").checked == true) ? "localStorage" : "sessionStorage";
+
+////    if (storageName in window && window[storageName] != null) {
+////        return window[storageName];
+////    } else {
+////        return null;
+////    }
+
+////}
+
+////function save() {
+////    let storage = getStorage();
+////    let key = document.getElementById("storageKey").value;
+////    let value = document.getElementById("storageValue").value;
+////    storage.setItem(key, value);
+////}
